b7badd71aab26c07166d5055ca8adee25420b47e
2sh <git@2sh.me>
2025-07-09 14:54:34 +0100

    Add lesser doxology wip

--- src/scripts/alpine-init.ts
+++ src/scripts/alpine-init.ts
@@ -1,5 +1,19 @@
 import Alpine from 'alpinejs'
+import persist from '@alpinejs/persist'
+
+Alpine.plugin(persist)
+
 // @ts-ignore
 window.Alpine = Alpine
+
+const languageNames = new Intl.DisplayNames(['en'], {
+  type: 'language'
+});
+
+const general = Alpine.reactive({
+  languageNames
+})
+
+Alpine.store("general", general)
 
 Alpine.start()
