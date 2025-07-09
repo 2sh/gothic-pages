3861eba5864571e33a0ba19caf025e2034b38391
2sh <git@2sh.me>
2025-07-09 07:16:06 +0100

    Add init

---
+++ src/scripts/nicene-creed.ts
@@ -0,0 +1,10 @@
+import Alpine from 'alpinejs'
+// @ts-ignore
+window.Alpine = Alpine
+
+const tools = Alpine.reactive({
+
+})
+
+Alpine.store("tools", tools)
+Alpine.start()
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from "vite"
+
+import { join, resolve } from "path"
+import { sync } from "glob"
+
+import { ViteEjsPlugin } from "vite-plugin-ejs"
+
+export default defineConfig({
+  base: '',
+  root: join(__dirname, "src"),
+  build: {
+    outDir: join(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: sync(resolve(__dirname, "src", "*.html")),
+    },
+  },
+  plugins: [
+    ViteEjsPlugin()
+  ],
+  resolve: {
+    alias: {
+      '@client': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
