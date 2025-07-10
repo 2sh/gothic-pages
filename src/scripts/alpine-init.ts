e81160436792be624a0dabd0e6f38f26f4ef5ba0
2sh <git@2sh.me>
2025-07-10 10:33:54 +0100

    Make dark mode global and make light-mode the class
    With dark mode the default, making light-mode the optional class to prevent bright flashes between loads

--- src/scripts/alpine-init.ts
+++ src/scripts/alpine-init.ts
@@ -11,8 +11,18 @@ const languageNames = new Intl.DisplayNames(['en'], {
 });
 
 const general = Alpine.reactive({
-  languageNames
+  languageNames,
+  darkMode: Alpine.$persist(
+    window.matchMedia
+    && window.matchMedia('(prefers-color-scheme: dark)').matches
+  ).as("dark_mode")
 })
+
+window.matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', event => {
+    // @ts-ignore this works but the TS def isn't right
+    general.darkMode = !!event.matches;
+  });
 
 Alpine.store("general", general)
 
