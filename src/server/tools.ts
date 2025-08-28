d06b2d9dff1862e86da362154b8e6148d181893d
2sh <git@2sh.me>
2025-08-28 09:53:09 +0100

    Improve menu icons

--- src/server/tools.ts
+++ src/server/tools.ts
@@ -93,10 +93,21 @@ const htmlInfoBox = html`<template x-if="selectedLineInfo">
   </div>
 </template>`
 
+// SVG icons taken from https://heroicons.com/mini
 
-const darkModeButton = html`<button class='simple-text square'
-  x-text="$store.general.darkMode ? '☀' : '☾'"
-  @click="$store.general.darkMode = !$store.general.darkMode"></button>`
+const darkModeButton = html`<button
+  @click="$store.general.darkMode = !$store.general.darkMode">
+  <template x-if='$store.general.darkMode'>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+      <path d="M10 2a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 2ZM10 15a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 15ZM10 7a3 3 0 1 0 0 6 3 3 0 0 0 0-6ZM15.657 5.404a.75.75 0 1 0-1.06-1.06l-1.061 1.06a.75.75 0 0 0 1.06 1.06l1.06-1.06ZM6.464 14.596a.75.75 0 1 0-1.06-1.06l-1.06 1.06a.75.75 0 0 0 1.06 1.06l1.06-1.06ZM18 10a.75.75 0 0 1-.75.75h-1.5a.75.75 0 0 1 0-1.5h1.5A.75.75 0 0 1 18 10ZM5 10a.75.75 0 0 1-.75.75h-1.5a.75.75 0 0 1 0-1.5h1.5A.75.75 0 0 1 5 10ZM14.596 15.657a.75.75 0 0 0 1.06-1.06l-1.06-1.061a.75.75 0 1 0-1.06 1.06l1.06 1.06ZM5.404 6.464a.75.75 0 0 0 1.06-1.06l-1.06-1.06a.75.75 0 1 0-1.061 1.06l1.06 1.06Z" />
+    </svg>
+  </template>
+  <template x-if='!$store.general.darkMode'>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+      <path fill-rule="evenodd" d="M7.455 2.004a.75.75 0 0 1 .26.77 7 7 0 0 0 9.958 7.967.75.75 0 0 1 1.067.853A8.5 8.5 0 1 1 6.647 1.921a.75.75 0 0 1 .808.083Z" clip-rule="evenodd" />
+    </svg>
+  </template>
+</button>`
 
 const modeSelector = (includeModes: string[]) => html`<select x-model="mode">
   <option value="simple">𐌰𐌹𐌽𐍆𐌰𐌸𐍃</option>
@@ -137,7 +148,11 @@ export function createArticleBody(content: string, langs: string[] = ['got'], in
       <div id="article-inner">
         <div class="menu">
           <div>
-            <a class='simple-text square' href="index.html">⌂</a>
+            <a href="index.html">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z" clip-rule="evenodd" />
+              </svg>
+            </a>
           </div>
           <div>
             ${darkModeButton}
