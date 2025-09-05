3fee0c54972e462e7772395e46e099aa5b73e1ca
2sh <git@2sh.me>
2025-09-05 15:42:19 +0100

    Add further TLs

--- src/server/pages/nicetas-the-goth.ts
+++ src/server/pages/nicetas-the-goth.ts
@@ -14,6 +14,8 @@ import
 // https://pemptousia.com/2022/09/%ce%b1-goth-saint-the-holy-martyr-nikitas-15-september/
 // https://www.saintsophiadc.org/%CE%B1-goth-saint-the-holy-martyr-nikitas/
 
+// https://www.worldhistory.org/Athanaric/
+
 global.lineId = 0
 
 const title = '𐌼𐌹𐌺𐌹𐌻𐌼𐌰𐍂𐍄𐍅𐍂 𐌽𐌹𐌺𐌴𐍄𐌰 𐍃𐌰 𐌲𐌿𐍄𐌰'
@@ -25,7 +27,7 @@ article += html`<header>
     got: 'Mikilmartwr Niketa sa Guta',
     en: "Great-Martyr Nicetas the Goth",
   },
-  notes: `Niketa (Nικήτας) following https://www.nthuleen.com/papers/755gothpaper.html for naming, n-stem without s as I can imagine it became the norm.`
+  notes: `Niketa (Nικήτας) following https://www.nthuleen.com/papers/755gothpaper.html for naming, n-stem without -s as I can imagine it became the norm.`
 },)}</h1>
 </header>`
 
@@ -225,6 +227,250 @@ article += html`<p>${[
   },
 ].map(toGothicLine).join('')}</p>`
 
+
+article += html`<h2>${toGothicLine({
+  text: {
+    got: 'Liuþ Weihamma Niketan',
+    en: "Hymn to Saint Niketas",
+  },
+  notes: "Romans 15:9 dative without preposition"
+})}</h2>`
+
+
+article += html`<p>
+${toGothicLine({
+  text: {
+    got: "Sa ïst ïnkunja sunjis, ïn þiudái allái,",
+    en: "He is a true patriot, among all the people,",
+  },
+  notes:
+`Although ïnkunja means fellow countryman, I feel like it works in this context.
+Zealot could maybe work too.
+
+Using þiuda to mean people of his tribe, a few bible verses use the word for heathen but mostly gentiles.`
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "saei gasatjiþ grunduwaddju hunslastadis weihis sunjis",
+    en: "who founds a real sacred altar,",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "wulþau Gudis Libandins.",
+    en: "to the glory of the Living God.",
+  },
+})}
+</p>`
+
+article += html`<p>
+${toGothicLine({
+  text: {
+    got: "Guta Aþanareiks reikinoda miþ anamahtái",
+    en: "The Goth Athanaric ruled with violence",
+  },
+  notes:
+``
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "jah salida hunsla galiugam ungawagjandam.",
+    en: "and offered sacrifices to the inanimate idols.",
+  },
+  notes: `"hunsla saljan" John 16:2; "galiugam saljada" Corinthians I 10:19.`
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "Ïþ Weihs Niketa was gadraúhts Xristáus",
+    en: "But Saint Nicetas was a soldier of Christ",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "jah merjands naseináis áiweináizos.",
+    en: "and a herald of eternal salvation.",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "Ussandida strelos Liuhadis Áiweinis þaírh naht,",
+    en: "He sent rays of the Eternal Light through the night,",
+  },
+  notes: '*strela [f. O] PGm *strēlō- (in Le Petit Prince)'
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "maltjandeins riqis galiugandan",
+    en: "dissolving the idolatrous gloom",
+  },
+  notes: `*gamaltjan from attested gamalteins
+
+using galiugands to mean idolatrous`
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "saei sáiwalos biwaibida swe hulistr.",
+    en: "which wrapped souls like a shroud.",
+  },
+})}
+</p>`
+
+article += html`<p>
+${toGothicLine({
+  text: {
+    got: "Weihs Niketa andstoþ þamma reik",
+    en: "Saint Nicetas stood against the prince",
+  },
+  notes: `Using reik referencing it in Aþanareiks`
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "jah muns tulgus seins sildaleiknoda ïn þiudái seinái.",
+    en: "and his firm resolve astonished his people.",
+  },
+  notes: `"sildaleiknan in allaim þaim galaubjandam" Thessalonians II 1:10`
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "Miþ mahtái Galgins Gariudis þata riqis þairhstagg",
+    en: "With the power of the Honourable Cross he pierced the darkness",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "jah þiuda alla seina miþ faurhtein Gudis fullida.",
+    en: "and filled all his people with the fear of God.",
+  },
+})}
+</p>`
+
+article += html`<p>
+${toGothicLine({
+  text: {
+    got: "Bloþ weih ïs was woþi,",
+    en: "His holy blood was the sweet,",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "roseiga ūhtwo dagis niujis,",
+    en: "rosy dawn of a new day,",
+  },
+  notes: `*rosa from Latin rosa + -eigs`
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "miþþanei ahma ïs du gardim himinis usstáig.",
+    en: "while his spirit ascended to the courts of heaven.",
+  },
+})}
+</p>`
+
+article += html`<p>
+${toGothicLine({
+  text: {
+    got: "Niketa was branniþs ïn fonin ingrimjamma,",
+    en: "Nicetas was burned in the dreadful fire,",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "ïþ und hina dag þata fon ni gafret ïna;",
+    en: "but to this day the fire has not consumed him;",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "láibos ïs sind gahailos gabaurganos!",
+    en: "his relics have been preserved intact!",
+  },
+})}
+</p>`
+
+article += html`<p>
+${toGothicLine({
+  text: {
+    got: "Þai gutans wesun daupidai ïn Sunja Xristáus",
+    en: "The Goths were baptized into the Truth of Christ",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "jah hazidedun Nikitan, sigisan seinana.",
+    en: "and praised Nikitas, their victor.",
+  },
+  notes: '*sigison from PGm *sigizōną, then sigisa with -a agent suffix'
+})}
+</p>`
+
+article += html`<p>
+${toGothicLine({
+  text: {
+    got: "",
+    en: "Saint Nikitas, voice of the trumpet of God!",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "",
+    en: "Brave martyr, true lover of your land.",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "",
+    en: "You have left behind the dwelling-places of the earth",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "",
+    en: "and now reside with the choirs of angels",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "",
+    en: "in the courts of God the King.",
+  },
+})}
+<br>
+${toGothicLine({
+  text: {
+    got: "",
+    en: "Pray for us, you who have boldness towards the King.",
+  },
+})}
+</p>`
+
+article += html`<p class='annotation'>
+${toGothicLine({
+  text: {
+    got: "",
+    en: "Saint Nikolai Velimirović, The Ochrid Prologue, September.",
+  },
+})}
+</p>`
 
 article += html`<p lang='en' class="annotation">
   <span class="nowrap">Nicetas the Goth</span>
