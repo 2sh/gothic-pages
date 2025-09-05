1012f78f971b00c52587232067150eab204138d6
2sh <git@2sh.me>
2025-09-05 15:45:07 +0100

    Set back to relative link

--- src/server/tools.ts
+++ src/server/tools.ts
@@ -93,7 +93,7 @@ export function createArticleBody(content: string, config?: ConfigArticleBody)
     ...config
   }
 
-  const homeButton = html`<a href="https://2sh.me/gothic/" lang='en' title='Home'>
+  const homeButton = html`<a href="./" lang='en' title='Home'>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
     <path fill-rule="evenodd" d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z" clip-rule="evenodd" />
   </svg>
