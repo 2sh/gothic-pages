73a77e6dec76ad5b42df8f9bec71e51e10f8b0b8
2sh <git@2sh.me>
2026-01-02 22:28:13 +0000

    Improve

--- src/server/pages/nicetas-the-goth.ts
+++ src/server/pages/nicetas-the-goth.ts
@@ -326,9 +326,9 @@ ${toGothicLines([{
     },
     notes: `“hunsla saljan” John 16:2; “galiugam saljada” Corinthians I 10:19.
 
-Using afguþ instead of an idol loan as afgudis is used later and that makes
+Using afguþ instead of an idol loan as afgudis is used later and that makes it
 reference back to this word nicely. Trying to create a nice sounding word
-for this poem for idolatrous instead of afgudis
+for this poem for adjective idolatrous instead of afgudis
 that uses an idol loan would overcomplicate this.`
   }], info)}
 <br>
