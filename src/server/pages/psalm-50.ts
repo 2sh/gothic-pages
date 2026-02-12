fa51b954295b546dcff812ab92bf63e775200904
2sh <git@2sh.me>
2026-02-12 13:35:25 +0000

    Improve notes

--- src/server/pages/psalm-50.ts
+++ src/server/pages/psalm-50.ts
@@ -198,6 +198,7 @@ looking at balwawesei, and
       en: "Cast me not away from thy presence; and remove not thy Holy Spirit from me.",
     },
     notes: `"ahman seinana weihana" Thessalonians I 4:8
+
 ἀντανέλῃς means something like to take in exchange/return,
 most translations just go with take/remove/nehmen though.`
   }], info)}</span>
