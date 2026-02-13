d442fe1b5250eb99e8b13863f204a9d30f4e3b94
2sh <git@2sh.me>
2026-02-13 12:54:50 +0000

    Add line

--- src/server/pages/psalm-50.ts
+++ src/server/pages/psalm-50.ts
@@ -263,10 +263,13 @@ declaring/telling part like in the various translations I've seen.`
 <br>
 <span class='verse'>${toGothicLines([{
     text: {
-      got: "...",
+      got: "Tawei þiuþ, Frauja, in leikainai Þeinamma du Sion, jah timrjaindau waddjjus Iairusalems:",
       grc: "ἀγάθυνον, Κύριε, ἐν τῇ εὐδοκίᾳ σου τὴν Σιών, καὶ οἰκοδομηθήτω τὰ τείχη ῾Ιερουσαλήμ·",
       en: "Do good, O Lord, to Sion in thy good pleasure; and let the walls of Jerusalem be built.",
     },
+    notes: `"bi leikainai wiljins seinis" Ephesians 1:5
+
+no imperative involved, just the 3rd person subjunctive as a "let something else be so" meaning John 14:27`
   }], info)}</span>
 <span class='verse'>${toGothicLines([{
     text: {
